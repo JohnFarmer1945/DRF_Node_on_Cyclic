5d0d60502fb5a68df1a75f5197897ce72e1c19a3
John_Farmer <john.farmer@gmx.net>
2023-08-04 14:53:13 +0200

    Browser Reiter zeigt aktuellen Title der Seite

--- routes/static_files.js
+++ routes/static_files.js
@@ -3,33 +3,48 @@ var router = express.Router();
 
 // Routes to all static files
 router.get("/", function (req, res) {
+  var fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
   res.render("start");
 });
 
 router.get("/Start", function (req, res) {
-  res.render("start");
+  var URL = String(req.originalUrl).substring(1);
+  res.render("start", { title: URL });
 });
 
 router.get("/Team", function (req, res) {
-  res.render("team");
+  var URL = String(req.originalUrl).substring(1);
+  res.render("team", { title: URL });
 });
+
 -router.get("/Technik", function (req, res) {
-  res.render("technik");
+  var URL = String(req.originalUrl).substring(1);
+  res.render("technik", { title: URL });
 });
+
 router.get("/Umgebung", function (req, res) {
-  res.render("umgebung");
+  var URL = String(req.originalUrl).substring(1);
+  res.render("umgebung", { title: URL });
 });
+
 router.get("/Todos", function (req, res) {
-  res.render("todos");
+  var URL = String(req.originalUrl).slice(1);
+  res.render("todos", { title: URL });
 });
+
 router.get("/MoreToDos", function (req, res) {
-  res.render("moretodos");
+  var URL = String(req.originalUrl).slice(1);
+  res.render("moretodos", { title: URL });
 });
+
 router.get("/EmergencyAndProcedure", function (req, res) {
-  res.render("emergencyprocedure");
+  var URL = String(req.originalUrl).slice(1);
+  res.render("emergencyprocedure", { title: URL });
 });
+
 router.get("/More", function (req, res) {
-  res.render("more");
+  var URL = String(req.originalUrl).slice(1);
+  res.render("more", { title: URL });
 });
 
 module.exports = router;
