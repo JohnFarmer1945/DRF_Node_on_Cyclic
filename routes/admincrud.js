c4b8544cb959ffcf7fb5f299b978df3cd4a3c7c5
JohnFarmer1945 <john.farmer@gmx.net>
2023-09-06 09:35:05 +0200

    admin READ DELETE Working

--- routes/admincrud.js
+++ routes/admincrud.js
@@ -5,11 +5,49 @@ const emergencyFlightModel = require('../models/emergencyProcedures')
 // Admin Routes
 router.get("/", async (request, response) => {
   const emergencyFlightModelRes = await emergencyFlightModel.find({});
-  console.log (emergencyFlightModelRes);
-  response.render("../views/adminpage");
+  emergencyFlightModelResReduced = [];
+  counter = 1;
+
+  for (const question of emergencyFlightModelRes) {
+    emergencyFlightModelResReduced.push(
+      {
+        mongo_id : question._id,
+        id : counter, 
+        name : question.name
+
+      }
+    );
+    counter +=1
+  }
+  console.log(emergencyFlightModelResReduced);
+  
+  response.render("../views/adminpage", {  emergencyFlightModelResReduced : emergencyFlightModelResReduced  });
 });
 
 
+router.get('/delete/:id', deleteAction);
+
+async function deleteAction (request, response) {
+  const MongoIDFromURLLessDollar = request.params.id.slice(1);
+  console.log(`request.params.id: ${MongoIDFromURLLessDollar}`)
+  try {
+    const questionToBeDeleted = await emergencyFlightModel.findByIdAndDelete(MongoIDFromURLLessDollar);
+    response.redirect("/admin");
+    if (!questionToBeDeleted) response.status(404).send("No item found");
+    response.status(200).send();
+  } catch (error) {
+    response.status(500).send(error);
+  }
+}
+
+
+// Export Modules
+module.exports = router;
+
+
+
+
+/*
 router.post("/create", async (request, response) => {
   const emergency = new emergencyFlightModel(request.body);
   console.log(emergency);
@@ -21,14 +59,7 @@ router.post("/create", async (request, response) => {
     response.status(500).send(error);
   }
 });
-
-
-
-module.exports = router;
-
-    
-   
-    
+*/    
 
 /*
 PASTE LATER FOR NAME IN TAB
